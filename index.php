d9296236834eb9b5aae544cfa085843eab198f37
sarah <sarahlakrouz@gmail.com>
2024-11-13 12:36:32 +0100

    modif du php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+require_once 'controllers/FilmsController.php';
+require_once 'controllers/ContactController.php';
+
+
+$requestURI = $_SERVER['REQUEST_URI'];
+
+
+switch ($requestURI) {
+    case '/films':
+        
+        $controller = new FilmsController();
+        $controller->readAll();
+        break;
+
+    case '/contact':
+        
+        $controller = new ContactController();
+        $controller->contact();
+        break;
+
+    
+}
