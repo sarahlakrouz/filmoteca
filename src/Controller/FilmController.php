56a928d018b233572c801bbab80f2bb3b7f8679f
sarah <sarahlakrouz@gmail.com>
2024-11-20 10:58:34 +0100

    Sauvegarde des modifications dans le projet_web

---
+++ src/Controller/FilmController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+class FilmController
+{
+    public function create()
+    {
+    }
+
+    public function read()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
